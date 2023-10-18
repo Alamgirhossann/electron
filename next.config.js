c3370816aed0f3aed24288a39bcada96e236086d
Alamgirhossann <alamgirhossann@gmail.com>
2023-10-18 18:03:57 +0600

    nextjs config changed

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+    return config;
+  },
+  // images
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
