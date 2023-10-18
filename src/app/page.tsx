844699279752a4c918ace3ac3ea5d5e46695d168
Alamgirhossann <alamgirhossann@gmail.com>
2023-10-18 12:40:48 +0600

    home page changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,25 @@
-import Image from "next/image";
-import { redirect } from "next/navigation";
+"use client";
+
+import AboutUs from "@/components/ui/AboutUs";
+import ContactUs from "@/components/ui/ContactUs";
+import Footer from "@/components/ui/Footer";
+import Header from "@/components/ui/Header";
+import OurProject from "@/components/ui/OurProject";
+import Services from "@/components/ui/Services";
+import TestimonialCarousel from "@/components/ui/Testimonial";
+import TopBanner from "@/components/ui/TopBanner";
 
 export default function Home() {
-  return redirect("/home");
+  return (
+    <div>
+      <Header />
+      <TopBanner />
+      <Services />
+      <OurProject />
+      <AboutUs />
+      <TestimonialCarousel />
+      <ContactUs />
+      <Footer />
+    </div>
+  );
 }
--- src/components/ui/OurProject.tsx
+++ src/components/ui/OurProject.tsx
@@ -2,29 +2,24 @@ import React from "react";
 import { Card, Col, Row } from "antd";
 import Image from "next/image";
 // import "antd/dist/antd.css"; // Import Ant Design styles
-import heading_icon from "../../assets/heading_icon.png";
-import project_img1 from "../../assets/project_img1.jpg";
-import project_img2 from "../../assets/project_img2.jpg";
-import project_img3 from "../../assets/project_img3.jpg";
+// import heading_icon from "../../assets/heading_icon.png";
 
 const cardData = [
-  { title: "MODERN FIXTURE", image: project_img1 },
-  { title: "VINTAGE CHANDLIER", image: project_img2 },
-  { title: "BULPILO", image: project_img3 },
+  { title: "MODERN FIXTURE" },
+  { title: "VINTAGE CHANDLIER" },
+  { title: "BULPILO" },
 ];
 
-const OurProject = () => {
+const Projects = () => {
   return (
     <div className="bg-gray-200 py-7 px-2" id="project">
       <h1 className="flex justify-center md:text-3xl text-xl font-bold mb-4 pt-3">
-        <Image src={heading_icon} alt="heading_icon" width={20} height={15} />
         <span className="ms-3">Our Projects</span>
       </h1>
       <Row gutter={16}>
         {cardData.map((data, index) => (
           <Col xs={24} sm={12} md={8} key={index} className="mb-3">
             <Card className=" shadow-2xl h-full ">
-              <Image src={data.image} alt="project_image" width={500} />
               <p className="md:text-3xl text-xl text-center">{data.title}</p>
             </Card>
           </Col>
@@ -34,4 +29,4 @@ const OurProject = () => {
   );
 };
 
-export default OurProject;
+export default Projects;
