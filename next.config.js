a24b7d2aea833dc3cd203add62edfcf9147e37c0
Alamgirhossann <alamgirhossann@gmail.com>
2023-10-19 15:34:06 +0600

    config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: "/about",
-        destination: "/",
-      },
-    ];
-  },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+module.exports = nextConfig;
