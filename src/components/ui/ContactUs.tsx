bc2c95497c99a2e5911b36d913bfcdc00cebf839
Alamgirhossann <alamgirhossann@gmail.com>
2023-11-25 23:10:16 +0600

    emailjs added

--- src/components/ui/ContactUs.tsx
+++ src/components/ui/ContactUs.tsx
@@ -8,11 +8,41 @@ import heading_icon from "../../assets/heading_icon.png";
 import FormTextArea from "../forms/FormTextArea";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { contactFormSchema } from "@/schemas/formValidationSchema";
-
+import emailjs from "emailjs-com";
+emailjs.init("YOUR_USER_ID");
+import { useState } from "react";
 const ContactUs = () => {
-  const onSubmit = async (data: any) => {
-    message.success("your data submitted successfully");
+  const [formData, setFormData] = useState({
+    name: "",
+    phoneNumber: "",
+    email: "",
+    message: "",
+  });
+  const onSubmit = () => {
+    emailjs
+      .sendForm(
+        "service_lr1ih5h",
+        "template_mxr2j6r",
+
+        "7GU6olgrht7sINqSx"
+      )
+      .then(
+        (result) => {
+          console.log("Email sent successfully:", result.text);
+          setFormData({
+            name: "",
+            phoneNumber: "",
+            email: "",
+            message: "",
+          });
+          message.success("your data submitted successfully");
+        },
+        (error) => {
+          console.error("Email send failed:", error);
+        }
+      );
   };
+
   return (
     <div className="flex justify-center mt-5 px-2" id="contact">
       <div className="md:w-2/4 w-full">
@@ -28,22 +58,31 @@ const ContactUs = () => {
         >
           <Row>
             <Col span={24} style={{ margin: "10px 0" }}>
-              <FormInput name="name" label="Name" />
+              <FormInput name="name" label="Name" value={formData.name} />
             </Col>
           </Row>
           <Row>
             <Col span={24} style={{ margin: "10px 0" }}>
-              <FormInput name="phoneNumber" label="Phone Number" />
+              <FormInput
+                name="phoneNumber"
+                label="Phone Number"
+                value={formData.phoneNumber}
+              />
             </Col>
           </Row>
           <Row>
             <Col span={24} style={{ margin: "10px 0" }}>
-              <FormInput name="email" label="Email" />
+              <FormInput name="email" label="Email" value={formData.email} />
             </Col>
           </Row>
           <Row>
             <Col span={24} style={{ margin: "10px 0" }}>
-              <FormTextArea name="message" label="Message" rows={4} />
+              <FormTextArea
+                name="message"
+                label="Message"
+                rows={4}
+                value={formData.message}
+              />
             </Col>
           </Row>
 
